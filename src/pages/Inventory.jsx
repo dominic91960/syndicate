b031372e5c42bb0f2ff0030b9c8020e2ba479b6d
Dominic Brian <dominic91960@gmail.com>
2025-03-04 03:17:06 +0530

    fix(dashboard): minor changes

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,9 +1,14 @@
-import PageTitle from "../components/dashboard/PageTitle";
+// import PageTitle from "../components/dashboard/PageTitle";
 
 const InventoryPage = () => {
   return (
     <div className="h-full w-full">
-      <PageTitle title="Inventory" />
+      {/* <PageTitle title="Inventory" /> */}
+      <iframe
+        src="https://d2mlmfod4h1sc4.cloudfront.net/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Dashboard"
+      ></iframe>
     </div>
   );
 };
